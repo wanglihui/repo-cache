2a4caab5828cac935ae6513fb642210f0c9cb0c8
wanglihui <lihui@guanghe.com>
2022-10-24 16:23:44 +0800

    add redis storage client

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/wanglihui/repo-cache
 go 1.19
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	golang.org/x/exp v0.0.0-20220921164117-439092de6870 // indirect
 	golang.org/x/sync v0.0.0-20220907140024-f12130a52804 // indirect
 )
---
+++ storage/redis.go
@@ -0,0 +1,47 @@
+package storage
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func NewRedisStorage(prefix string, c *redis.Client) StorageInterface {
+	return &RedisStorage{
+		prefix: prefix,
+		client: c,
+	}
+}
+
+func (it *RedisStorage) Get(ctx context.Context, key Key) (Value, error) {
+	fullKey := it.getKey(key)
+	var ret Value
+	val, err := it.client.Get(ctx, fullKey).Result()
+	if err != nil && err != redis.Nil {
+		return ret, err
+	}
+	if err == redis.Nil {
+		return ret, nil
+	}
+	ret = Value(val)
+	return ret, nil
+}
+
+func (it *RedisStorage) Set(ctx context.Context, key Key, val Value) error {
+	fullKey := it.getKey(key)
+	return it.client.Set(ctx, fullKey, val, time.Hour).Err()
+}
+
+func (it *RedisStorage) Delete(ctx context.Context, key Key) error {
+	fullKey := it.getKey(key)
+	return it.client.Del(ctx, fullKey).Err()
+}
+func (it *RedisStorage) getKey(key Key) string {
+	return it.prefix + ":" + string(key)
+}
+
+type RedisStorage struct {
+	prefix string
+	client *redis.Client
+}
